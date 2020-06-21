971a7b5976ce6a90e72a7dc0965bf0272ec00e71
Alish Giri <alishgiri.92@gmail.com>
2020-06-21 18:28:46 +0545

    Use transform to play around with Matrix4

--- lib/animations/transform_animation.dart
+++ lib/animations/transform_animation.dart
@@ -5,12 +5,59 @@ class TransformAnimation extends StatefulWidget {
 }
 
 class _TransformAnimation extends State<TransformAnimation> {
+  double x = 0.0;
+  double y = 0.0;
+  double z = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Transform Animation')),
-      body: Column(
-        children: <Widget>[],
+      appBar: AppBar(title: Text('Transform Matrix4')),
+      body: Center(
+        child: Transform(
+          alignment: FractionalOffset.center,
+          transform: Matrix4(
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+            0,
+            0,
+            0,
+            0,
+            1,
+          )
+            ..rotateX(x)
+            ..rotateY(y)
+            ..rotateZ(z),
+          child: GestureDetector(
+            onPanUpdate: (DragUpdateDetails details) {
+              setState(() {
+                y -= details.delta.dx / 100;
+                x += details.delta.dy / 100;
+              });
+            },
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.blueGrey,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Move Me',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
