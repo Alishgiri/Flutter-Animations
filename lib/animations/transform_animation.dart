9331e04a548867f9f618a561cca846fc1be8400a
Alish Giri <alishgiri.92@gmail.com>
2020-06-21 16:15:22 +0545

    Setup app from experimenting with animations

---
+++ lib/animations/transform_animation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TransformAnimation extends StatefulWidget {
+  _TransformAnimation createState() => _TransformAnimation();
+}
+
+class _TransformAnimation extends State<TransformAnimation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Transform Animation')),
+      body: Column(
+        children: <Widget>[],
+      ),
+    );
+  }
+}
